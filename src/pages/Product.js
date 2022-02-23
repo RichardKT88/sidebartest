817a1f63cfe15ee84f87f82bfb44fc01f41d3013
Richard Kendy Tanaka <kendy_t88@hotmail.com>
2022-02-23 20:11:03 -0300

    First commit

---
+++ src/pages/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../components/Navbar.css';
+
+export default function Product()  {
+    return (
+        
+            <h1>Seus Produtos</h1>
+          
+       
+    )
+}
---
+++ src/pages/Report.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Report() {
+    return (
+        <div>
+            <h1>Seus Reports</h1>
+        </div>
+    )
+}
